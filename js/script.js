698c98cb4df8da16841ceea6a8371c59bfed2e1a
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-08-04 13:02:58 +0600

    Refactor code and added more interactivities

--- js/script.js
+++ js/script.js
@@ -20,17 +20,17 @@ const getMeals = (e) => {
 					const meals = data.meals.splice(0, 8);
 					meals.forEach((meal) => {
 						html += `
-                        <button class="meal-card-btn" onclick="getMealDetails(${meal.idMeal})">
-                            <div class="meal-card" data-id="${meal.idMeal}">
-                                <img
-                                    class="meal-card-img"
-                                    src="${meal.strMealThumb}"
-                                    alt="Recipe image"
-                                />
-                                <h3 class="meal-card-title">${meal.strMeal}</h3>
-                            </div>
-                        </button>
-                    `;
+                            <button class="meal-card-btn" onclick="getMealDetails(${meal.idMeal})">
+                                <div class="meal-card" data-id="${meal.idMeal}">
+                                    <img
+                                        class="meal-card-img"
+                                        src="${meal.strMealThumb}"
+                                        alt="Recipe image"
+                                    />
+                                    <h3 class="meal-card-title">${meal.strMeal}</h3>
+                                </div>
+                            </button>
+                        `;
 					});
 					mealWrapper.classList.remove('not-found');
 				} else {
@@ -64,50 +64,21 @@ const getMealDetails = (id) => {
 
 // displaying modal after click on meal-card
 const hungryMonsterModalDisplay = (meal) => {
+	console.log(meal);
 	hungryMonsterModal.style.display = 'block';
 	let html = `
-        <div class="hungry-monster-modal-header">
-            <i onclick="modalClose()" class="fas fa-times modal-close-icon"></i>
-        </div>
-        <img
-            class="hungry-monster-modal-img"
-            src="${meal.strMealThumb}"
-        />
-        <h2 class="modal-first-title">${meal.strMeal}</h2>
-        <h3 class="modal-second-title">Ingredients</h3>
-
-        <ul class="modal-list">
-            <li class="modal-list-item">
-                <span>
-                    <i class="fas fa-check-square modal-ingredient-icon"></i>
-                </span>
-                ${meal.strIngredient1}
-            </li>
-            <li class="modal-list-item">
-                <span>
-                    <i class="fas fa-check-square modal-ingredient-icon"></i>
-                </span>
-                ${meal.strIngredient2}
-            </li>
-            <li class="modal-list-item">
-                <span>
-                    <i class="fas fa-check-square modal-ingredient-icon"></i>
-                </span>
-                ${meal.strIngredient3}
-            </li>
-            <li class="modal-list-item">
-                <span>
-                    <i class="fas fa-check-square modal-ingredient-icon"></i>
-                </span>
-                ${meal.strIngredient4}
-            </li>
-            <li class="modal-list-item">
-                <span>
-                    <i class="fas fa-check-square modal-ingredient-icon"></i>
-                </span>
-                ${meal.strIngredient5}
-            </li>
-        </ul>
+                <div class="hungry-monster-modal-header">
+                    <i onclick="modalClose()" class="fas fa-times modal-close-icon"></i>
+                </div>
+                <img
+                    class="hungry-monster-modal-img"
+                    src="${meal.strMealThumb}"
+                />
+                <h2 class="modal-first-title">${meal.strMeal}</h2>
+                <h3 class="modal-second-title">Instructions</h3>
+                <div class="modal-body">
+                    <p>${meal.strInstructions}</p>
+                </div>
     `;
 	hungryMonsterModal.innerHTML = html;
 };
